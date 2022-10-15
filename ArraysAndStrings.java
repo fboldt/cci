2a97f8d867f82d0fc708ba340bd4728d485cc45d
Francisco de Assis Boldt <fboldt@gmail.com>
2022-10-15 14:48:59 -0300

    add question 2 from chapter 1

--- ArraysAndStrings.java
+++ ArraysAndStrings.java
@@ -1,22 +1,45 @@
-import java.util.Arrays;
-
 public class ArraysAndStrings {
-    
     /**
-     * 1.1 Check is a string has only unique characters.
+     * 1.2 Check if one string is a permutation of other. *
      * 
-     * @param str
-     *            String to be checked
-     * @return
-     *         true when there is no repeated character
+     * @param str1
+     * @param str2
+     * @return true when str2 is a permutation of str1.
      */
-    public static boolean isUnique(String str) {
-        char[] sorted = str.toCharArray(); // O(n)
-        Arrays.sort(sorted); // O(n log n) <---dominant
-        for (int i = 0; i < sorted.length - 1; i++) { // O(n)
-            if (sorted[i] == sorted[i + 1]) {
+    public static boolean checkPermutation(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        int[] letters = new int[128];
+        for (int i = 0; i < str1.length(); i++) {
+            letters[str1.charAt(i)]++;
+            letters[str2.charAt(i)]--;
+        }
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] != 0) {
                 return false;
             }
+        }
+        return true;
+    }
+
+    /**
+     * 1.1 Check is a string has only unique characters. *
+     * 
+     * @param str
+     * @return true when there is no repeated character.
+     */
+    public static boolean isUnique(String str) {
+        if (str.length() > 128) {
+            return false;
+        }
+        boolean[] charAt = new boolean[128];
+        for (int i = 0; i < str.length(); i++) {
+            char asciiValue = str.charAt(i);
+            if (charAt[asciiValue]) {
+                return false;
+            }
+            charAt[asciiValue] = true;
         }
         return true;
     }
--- ArraysAndStringsTest.java
+++ ArraysAndStringsTest.java
@@ -1,10 +1,30 @@
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
 
 public class ArraysAndStringsTest {
+
+    /** 
+     * 1.2
+     */
     @Test
-    public void testIsUnique(){
+    public void testCheckPermutation() {
+        assertFalse(ArraysAndStrings.checkPermutation("abc", "abcde"));
+        assertTrue(ArraysAndStrings.checkPermutation("god", "dog"));
+        assertTrue(ArraysAndStrings.checkPermutation("Check if one string is a permutation of other.",
+                "Check if other string is a permutation of one."));
+        assertFalse(ArraysAndStrings.checkPermutation("Check if one string is a permutation of other.",
+                "Check if other string is a permutation of one. "));
+    }
+
+    /**
+     * 1.1
+     */
+    @Test
+    public void testIsUnique() {
         assertFalse(ArraysAndStrings.isUnique("banana"));
+        assertTrue(ArraysAndStrings.isUnique("abcdef"));
+        assertTrue(ArraysAndStrings.isUnique(""));
     }
 }
